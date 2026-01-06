2311fff18612574ac96943375ab6d82c20fd9357
Jorge Mabucuro <jorgeafonso@gmail.com>
2026-01-07 00:44:43 +0200

    coregindo caminho da pasta

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
-import Products from "./components/products/Products"
+import Products from "./components/Products/Products"
 import Info from "./components/Info/Info"
 import Contact from "./components/Contact/Contact"
 import Footer from "./components/Footer/Footer"
