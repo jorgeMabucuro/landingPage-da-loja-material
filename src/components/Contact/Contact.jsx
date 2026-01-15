04f2a06a85c84a9d6d83a70c22d7c3bd31eaa950
Jorge Mabucuro <jorgeafonso@gmail.com>
2026-01-16 00:09:42 +0200

    actualizar as cordenadas no mapa

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -31,7 +31,7 @@ function Contact() {
       <div className="contact-map">
         <iframe
           title="Mapa da Loja"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14458.123456789!2d32.573!3d-25.965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1ee6b123456789%3A0xabcdef123456789!2sBairro%20do%20Agostinho%20Neto%2C%20Maputo!5e0!3m2!1spt!2smz!4v1700000000000"
+          src="https://www.google.com/maps?q=-25.774405,32.608148&output=embed"
           width="100%"
           height="350"
           style={{ border: 0 }}
